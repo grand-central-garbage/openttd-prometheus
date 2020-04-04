8b8e7a3edad17ea0905dd41e4f0eb4f5db4d26c5
Louis Taylor <louis@kragniz.eu>
2020-04-04 17:51:24 +0100

    Add game name label
    (cherry picked from commit 3dc0dac03f92fc2ae6571eb23b732216e9b2736b)

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -1,5 +1,10 @@
 #include "metrics.h"
 
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
 #include "cargotype.h"
 #include "stdafx.h"
 #include "strings_func.h"
@@ -9,6 +14,8 @@ namespace prom {
 using namespace prometheus;
 
 auto prometheus_registry = std::make_shared<Registry>();
+
+std::string game_name;
 
 Family<Counter> &income_counter_family =
     BuildCounter()
@@ -37,28 +44,41 @@ Family<Counter> &cargo_delivered_income_family =
 void RegisterMetrics() {
   static Exposer exposer{"127.0.0.1:10808", "/metrics", 1};
   exposer.RegisterCollectable(prometheus_registry);
+
+  auto t = std::time(nullptr);
+  auto tm = *std::localtime(&t);
+
+  std::ostringstream oss;
+  oss << std::put_time(&tm, "openttd-%d-%m-%Y-%H-%M-%S");
+  game_name = oss.str();
 }
 
 CompanyMetrics::CompanyMetrics(uint16 name_1) {
   this->name = name_1;
   auto company_name = std::to_string(this->name);
 
-  this->income_counter = std::shared_ptr<prometheus::Counter>(
-      &income_counter_family.Add({{"company", company_name}}));
+  this->income_counter =
+      std::shared_ptr<prometheus::Counter>(&income_counter_family.Add(
+          {{"game", game_name}, {"company", company_name}}));
 
-  this->expenses_counter = std::shared_ptr<prometheus::Counter>(
-      &expenses_counter_family.Add({{"company", company_name}}));
+  this->expenses_counter =
+      std::shared_ptr<prometheus::Counter>(&expenses_counter_family.Add(
+          {{"game", game_name}, {"company", company_name}}));
 
   const CargoSpec *cargo;
   FOR_ALL_CARGOSPECS(cargo) {
     auto cargo_type = std::string(GetStringPtr(cargo->name));
     this->cargo_delivered_counters[cargo->label] =
-        std::shared_ptr<prometheus::Counter>(&cargo_delivered_family.Add(
-            {{"company", company_name}, {"cargo_type", cargo_type}}));
+        std::shared_ptr<prometheus::Counter>(
+            &cargo_delivered_family.Add({{"game", game_name},
+                                         {"company", company_name},
+                                         {"cargo_type", cargo_type}}));
 
     this->cargo_delivered_income_counters[cargo->label] =
-        std::shared_ptr<prometheus::Counter>(&cargo_delivered_income_family.Add(
-            {{"company", company_name}, {"cargo_type", cargo_type}}));
+        std::shared_ptr<prometheus::Counter>(
+            &cargo_delivered_income_family.Add({{"game", game_name},
+                                                {"company", company_name},
+                                                {"cargo_type", cargo_type}}));
   }
 
   std::cout << "creatign metrics" << this->name << std::endl;
